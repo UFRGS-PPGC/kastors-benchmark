053d3d82bef32391bd31afa633b31d9d11c9d9a9
Nathalie Furmento <nathalie.furmento@labri.fr>
2014-07-02 14:17:19 +0200

    autogen.sh: libtool is not needed

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-if ! libtool --version > /dev/null
-then
-	# Perhaps we are on a Mac
-	if ! glibtool --version > /dev/null
-	then
-	echo "GNU Libtool is missing, please install it."
-	exit 1
-	fi
-fi
 autoreconf -ivf -I m4
+
 
