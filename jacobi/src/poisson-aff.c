6b0bb71368c2534ca5eb028757a6157e098aaef3
Philippe Virouleau <philippe.virouleau@imag.fr>
2016-08-18 14:39:44 +0200

    Update affinity kinds to match the paper.

--- jacobi/src/poisson-aff.c
+++ jacobi/src/poisson-aff.c
@@ -127,7 +127,7 @@ double run(struct user_parameters* params)
         for (j = 0; j < ny; j+= block_size)
             for (i = 0; i < nx; i+= block_size)
             {
-#pragma omp task firstprivate(i,j) private(ii,jj) affinity(core:GET_PARTITION(i, j, block_size, nx, ny, num_thread), 1)
+#pragma omp task firstprivate(i,j) private(ii,jj) affinity(thread:GET_PARTITION(i, j, block_size, nx, ny, num_thread), 1)
                {
                 for (jj=j; jj<j+block_size; ++jj)
                     for (ii=i; ii<i+block_size; ++ii)
@@ -258,7 +258,7 @@ void rhs(int nx, int ny, double *f_, int block_size)
            int loc = GET_PARTITION(i, j, block_size, nx, ny, num_thread);
              printf("%2i ", loc );
 #endif
-#pragma omp task firstprivate(block_size,i,j,nx,ny) private(ii,jj,x,y) affinity(core:GET_PARTITION(i, j, block_size, nx, ny, num_thread), 1)
+#pragma omp task firstprivate(block_size,i,j,nx,ny) private(ii,jj,x,y) affinity(thread:GET_PARTITION(i, j, block_size, nx, ny, num_thread), 1)
             for (jj=j; jj<j+block_size; ++jj)
             {
                 y = (double) (jj) / (double) (ny - 1);
